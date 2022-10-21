80e195d078f5650af313916a108fc01f713e0ff5
Toàn Nguyễn <toannguyen@Toans-MacBook-Air.local>
2022-10-21 19:59:17 +0700

    first commit

---
+++ bai02/bai02_cach1.php
@@ -0,0 +1,14 @@
+<?php
+    $lang = 'PHP';
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bài 02</title>
+</head>
+<body>
+    <h1><?php echo 'Giới thiệu về '.$lang;?></h1>
+</body>
+</html>
